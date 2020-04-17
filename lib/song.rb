af8efd1bc89a1ef673d06de0b682e724861411b0
chic-caller-7520 <arbrown110@gmail.com>
2020-04-17 17:24:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,11 +11,11 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    binding.pry
     artist,song  = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song.save
+    binding.pry
   end
 
   def self.find_by_artist(artist)
